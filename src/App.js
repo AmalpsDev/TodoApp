b6687a35073507c68d0e30e9aec20d190fdb53d3
Amal <AmalpsDev>
2023-12-03 13:42:02 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import TodoApp from './components/TodoApp/TodoApp'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <TodoApp/>
+      </div>
+    )
+  }
+}
